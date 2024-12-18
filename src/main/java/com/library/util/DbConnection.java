aca76034627ba5081017a995a8fb7060f2c12c9f
Hafsa Ougha <hafsaougha17@gmail.com>
2024-12-18 23:07:55 +0100

    configure sonarqube on jenkins file

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -1,15 +1,45 @@
 package com.library.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbConnection {
-    private static final String URL = "jdbc:mysql://host.docker.internal:3307/library_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+    Properties props = new Properties();
+    FileInputStream fis = new FileInputStream("config.properties");
+
+//    private static final String URL = "jdbc:mysql://host.docker.internal:3307/library_db";
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "";
+
+    static{
+        Properties props = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream("config.properties");
+            props.load(fis);
+
+            URL = props.getProperty("DB.URL");
+            USER = props.getProperty("DB.USER");
+            PASSWORD = props.getProperty("DB.PASSWORD");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public DbConnection() throws FileNotFoundException {
+    }
 
     public static Connection getConnection() throws SQLException {
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("bien connecte");
